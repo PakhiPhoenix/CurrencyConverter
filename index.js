b2f88c37b9a69c51cf9802e761b7f75f399bb3a0
Deepakshi <deepakshi7vermani@gmail.com>
2025-09-01 15:28:48 +0530

    add API calling

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 let sel1 = document.querySelector("#countryInput");
 let sel2 = document.querySelector("#countryOutput");
+let theform = document.querySelector("#theForm");
 
-
+let inputCountry;
+let outputCountry;
 
 for (let i = 0; i < arr.length; i++) {
-
-    let opt1 = new Option(arr[i], arr[i]);
-    let opt2 = new Option(arr[i], arr[i]);
-    sel1.add(opt1);
-    sel2.add(opt2);
-//      opt[i].innerText = arr[i];
-//   opt[i].value = arr[i];
-//     console.log(opt[i].value);
-
+  let opt1 = new Option(arr[i], arr[i]);
+  let opt2 = new Option(arr[i], arr[i]);
+  sel1.add(opt1);
+  sel2.add(opt2);
 }
 
-
- //sel1.addEventListener("change", changeName(sel1.selected));
 sel1.addEventListener("change", () => {
-    changeInput(sel1.value);
+  changeInput(sel1.value);
 });
-
 
 let changeInput = (val) => {
-    console.log(val);
-
-}
+  inputCountry = val;
+};
 sel2.addEventListener("change", () => {
     changeOutput(sel2.value);
+
 });
 
-
 let changeOutput = (val) => {
-    console.log(val);
 
+    outputCountry = val;
+    console.log(outputCountry);
+};
+
+
+theform.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("page submitted without reloading");
+    getCurrency();
+})
+
+async function getCurrency() {
+
+
+    try {
+        const res = await fetch(
+          "https://v6.exchangerate-api.com/v6/0624643e7f549a5386b0c65b/latest/USD"
+        );
+        const data = await res.json();
+        console.log(data.conversion_rates[outputCountry]);
+    }
+
+    catch (error) {
+        console.log(error);
+    }
 }
+
+
+//getCurrency();
