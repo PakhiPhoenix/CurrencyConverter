a80560065393bcd1a0c1a95d783a1581c9b7f958
Deepakshi <deepakshi7vermani@gmail.com>
2025-09-02 18:45:00 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let sel1 = document.querySelector("#countryInput");
 let sel2 = document.querySelector("#countryOutput");
 let theform = document.querySelector("#theForm");
+let Amount = document.querySelector("#Amount");
+let ans = document.querySelector("p");
+let amount;
 
 let inputCountry="USD";
-let outputCountry;
+let outputCountry='USD';
+
+
+ // Populating the options
 
 for (let i = 0; i < arr.length; i++) {
   let opt1 = new Option(arr[i], arr[i]);
@@ -11,6 +17,20 @@ for (let i = 0; i < arr.length; i++) {
   sel1.add(opt1);
   sel2.add(opt2);
 }
+
+
+// Taking input of amount
+
+
+Amount.addEventListener("change", () => {
+  amount = Amount.innerText;
+  console.log(amount);
+});
+
+
+
+//Adding event listener on Currency
+
 
 sel1.addEventListener("change", () => {
   changeInput(sel1.value);
@@ -31,23 +51,33 @@ let changeOutput = (val) => {
 };
 
 
+// Form submission
+
 theform.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("page submitted without reloading");
+
+
     getCurrency();
 })
-console.log(
-  `https://v6.exchangerate-api.com/v6/0624643e7f549a5386b0c65b/latest/${inputCountry}`
-);
+
+
+// API call
+
 async function getCurrency() {
 
 
     try {
+        //  console.log("both values can't be same");
+        // if (inputCountry === outputCountry) {
+
+        //     alert("Both values can't be same");
+        // }
         const res = await fetch(
           `https://v6.exchangerate-api.com/v6/0624643e7f549a5386b0c65b/latest/${inputCountry}`
         );
         const data = await res.json();
         console.log(data.conversion_rates[outputCountry]);
+        ans.innerText = data.conversion_rates[outputCountry] * Amount.value + outputCountry;
     }
 
     catch (error) {
@@ -56,4 +86,5 @@ async function getCurrency() {
 }
 
 
-//getCurrency();
+
+
